3d56a7ce9a8fdb7f77ed9261cb183859e9dd7983
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-29 23:00:20 +0530

    hehe

--- server.py
+++ server.py
@@ -6,10 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    age= requests.get("https://api.agify.io?name=michael")
-    gender = requests.get("https://api.genderize.io?name=peter")
     return render_template("index.html",num=random.randint(1,10),year=datetime.date.today().year)
 
+@app.route('/guess/<name>')
+def guess(name):
+    #name comes from url which gets passed into the function
+    age_resposne= requests.get(f"https://api.agify.io?name={name}")
+    age_data=age_resposne.json()
+    age=age_data["age"]
+    gender_response = requests.get(f"https://api.genderize.io?name={name}")
+    gender_data=gender_response.json()
+    gender=gender_data["gender"]
+    return render_template("name.html",year=datetime.date.today().year,person_name=name,age=age,gender=gender)
+
 if __name__=="__main__":
-    
     app.run(debug=True)
