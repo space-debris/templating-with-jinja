595d7c456fe4b7ed02941d426968f3d58a6f12fd
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-29 22:26:01 +0530

    jinja

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 import random, datetime
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    age= requests.get("https://api.agify.io?name=michael")
+    gender = requests.get("https://api.genderize.io?name=peter")
     return render_template("index.html",num=random.randint(1,10),year=datetime.date.today().year)
 
 if __name__=="__main__":
