f9b144a0f9ec655bfe88d436e58ccc9d5e8132dd
Mayur Raj <mayur.13519011622@ipu.ac.in>
2024-01-29 22:18:12 +0530

    adding html

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import random, datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "hello world"
+    return render_template("index.html",num=random.randint(1,10),year=datetime.date.today().year)
 
 if __name__=="__main__":
     app.run(debug=True)
